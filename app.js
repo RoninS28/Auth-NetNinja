f62d2ed5139cadd120dcf0e5061c5852d41a6102
Rohan Shivesh <rvss28@gmail.com>
2022-02-10 18:31:16 +0530

    customization

--- app.js
+++ app.js
@@ -10,9 +10,9 @@ app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
 // database connection
-const dbURI = 'mongodb+srv://shaun:test1234@cluster0.del96.mongodb.net/node-auth';
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
-  .then((result) => app.listen(3000))
+const dbURI = 'mongodb+srv://bunty:bunty@jwtauth.omgiw.mongodb.net/AuthNetNinja?retryWrites=true&w=majority';
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+  .then((result) => app.listen(5000))
   .catch((err) => console.log(err));
 
 // routes
