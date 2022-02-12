1683ae678cd47e837c8cf528f41305018ea951db
Rohan Shivesh <rvss28@gmail.com>
2022-02-12 12:31:44 +0530

    add bcrypt and hooks

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const { isEmail } = require('validator')
+const bcrypt = require('bcrypt')
 
 const userSchema = new mongoose.Schema({
     email: {
@@ -14,6 +15,27 @@ const userSchema = new mongoose.Schema({
         required: [true, 'Please enter a password'],
         minlength: [6, 'Minimum password length is 6 characters']
     }
+})
+
+// Hooks \|/ (almost like db triggers)
+
+
+// fire a function before the foc saved to db
+userSchema.pre('save', async function (next) {
+    console.log('user about to be created and saved', this) //'this' is the doc to be saved ti the db
+
+    // password hashing
+    // we take the user password, add asalt to it and then pass it through the hashing algorithm
+    const salt = await bcrypt.genSalt()
+    this.password = await bcrypt.hash(this.password, salt)
+
+    next() //next takes the code to the next middleware
+})
+
+// fire a function after the foc saved to db
+userSchema.pre('save', function (doc, next) {//here doc is the mongodb doc from the db
+    console.log('user created and saved', doc) //'this' is the doc to be saved ti the db
+    next() //next takes the code to the next middleware
 })
 
 
